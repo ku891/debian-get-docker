e2c62e45c566eccb18d41a4b86cd1b348008d694
ku891 <69393540+ku891@users.noreply.github.com>
2025-04-13 01:00:38 +0800

    Create get-docker.sh

---
+++ get-docker.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install -y 'ca-certificates' 'curl'
+sudo install -m 0777 -d /etc/apt/keyrings
+echo '----------------------------------------------------------------------'
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc 
+echo '----------------------------------------------------------------------'
+chmod a+r /etc/apt/keyrings/docker.asc
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install -y 'docker-ce' 'docker-ce-cli' 'containerd.io' 'docker-buildx-plugin' 'docker-compose-plugin'
+echo
+echo
+echo 'docker and compose was installed now.'
+echo '----------------------------------------------------------------------'
+echo
+sudo docker -v
+echo
+echo
+sudo docker compose version
+echo
+echo
+echo
